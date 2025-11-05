df6fc5ac70777b9226369088092c4c72bdd2297e
smc4718 <smc4718@naver.com>
2025-11-05 17:35:10 +0900

    초기설정

---
+++ ArchiveWorld2/src/main/java/com/ArchiveWorld/ArchiveWorldApplication.java
@@ -0,0 +1,13 @@
+package com.ArchiveWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiveWorldApplication.class, args);
+	}
+
+}
